ccc6f561126f98a1f3b4c47208a22cec19d65a1d
lepo3002 <lepo3002@usherbrooke.ca>
2022-02-28 09:16:36 -0500

    return when no cam

--- main.py
+++ main.py
@@ -22,127 +22,140 @@ GLOBAL_PICTURE_DATA_DICT = {
     # "camera_gain": 0,
 }
 
-# Ui for the parameters
-app = QtWidgets.QApplication(sys.argv)
-set_param = MainWindow()
-app.exec_()
-if not set_param.data:
-    sys.exit()
-lambda_low = int(set_param.data[1])
-lambda_step = int(set_param.data[2])
-lambda_high = int(set_param.data[3])
-experience_name = set_param.data[0]
-exposure = int(set_param.data[4])
-gain = int(set_param.data[5])
-slit_ent = int(set_param.data[6])
-slit_exi = int(set_param.data[7])
-firstW = int(set_param.data[8])
 
-# Setting up the directory for the photos
-directory = set_param.working_dir + '\\ZonesNano\\'
-font = ImageFont.truetype('arial.ttf', 20)
+def Banc_Optique():
+    # Ui for the parameters
+    app = QtWidgets.QApplication(sys.argv)
+    set_param = MainWindow()
+    app.exec_()
+    if not set_param.data:
+        sys.exit()
+    lambda_low = int(set_param.data[1])
+    lambda_step = int(set_param.data[2])
+    lambda_high = int(set_param.data[3])
+    experience_name = set_param.data[0]
+    exposure = int(set_param.data[4])
+    gain = int(set_param.data[5])
+    slit_ent = int(set_param.data[6])
+    slit_exi = int(set_param.data[7])
+    firstW = int(set_param.data[8])
 
-# Part for data acquisition with Mono and Cam
-# If there is already photos to process, we dont acquire data
-if not set_param.crop.isChecked():
-    # TODO: Mettre le code pour la caméra et le monochromateur ici
-    print("_________ACQUISITION_EN_COURS_________")
-    directory = directory + 'ZonesNano/'
-    with Vimba.get_instance() as vimba:
-            cams = vimba.get_all_cameras()
-            with cams[0] as cam:
-                for wl in set_param.wavelengths:
+    # Setting up the directory for the photos
+    directory = set_param.working_dir + '\\ZonesNano\\'
+    font = ImageFont.truetype('arial.ttf', 20)
+
+    # Part for data acquisition with Mono and Cam
+    # If there is already photos to process, we dont acquire data
+    if not set_param.crop.isChecked():
+        # TODO: Mettre le code pour la caméra et le monochromateur ici
+        print("_________ACQUISITION_EN_COURS_________")
+        directory = directory + 'ZonesNano/'
+        with Vimba.get_instance() as vimba:
+                cams = vimba.get_all_cameras()
+                if not cams:
                     msg = QtWidgets.QMessageBox()
                     msg.setIcon(QtWidgets.QMessageBox.Warning)
                     msg.setText('Warning')
-                    msg.setInformativeText('Enter '+str(wl)+' nm in the spectro window')
+                    msg.setInformativeText('Camera not found - Returning to menu')
                     msg.setWindowTitle("Message")
                     msg.exec_()
-                    a = take_picture(cam, "test", "Photos", picture_number=str(wl))  
+                    return Banc_Optique()
+                with cams[0] as cam:
+                    for wl in set_param.wavelengths:
+                        msg = QtWidgets.QMessageBox()
+                        msg.setIcon(QtWidgets.QMessageBox.Warning)
+                        msg.setText('Warning')
+                        msg.setInformativeText('Enter '+str(wl)+' nm in the spectro window')
+                        msg.setWindowTitle("Message")
+                        msg.exec_()
+                        a = take_picture(cam, "test", "Photos", picture_number=float(wl))
 
 
-# If we have more than 1 zones to crop
-if set_param.multiple.isChecked():
-    nbZones = int(set_param.data[9])
-else:
-    nbZones = 1
-
-for j in range(nbZones):
-    # Variables
-    j = j+1
-    avg = []
-    wavelength = []
-
-    # Directories
-    input_loc = directory+str(firstW)+'.tiff'
-    try:
-        os.mkdir(directory + 'cropped')
-    except FileExistsError:
-        pass
-    output_loc = directory+'cropped\\'+'crop_'+str(firstW)+'nm.png'
-
-    # Setup and opening of the selection window
-    screen, px = setup(input_loc)
-    left, upper, right, lower = SelectionWindow(screen, px)
-
-    # Readjust the coordinates if they are reversed
-    if right < left:
-        left, right = right, left
-    if lower < upper:
-        lower, upper = upper, lower
-
-    # Doing the cropping
-    im = Image.open(input_loc)
-    im = im.crop((left, upper, right, lower))
-
-    # Exit the selection window
-    pygame.display.quit()
-
-    # Save the image in the output location
-    im.save(output_loc)
-
-    # Multiple zones identification on separate image
+    # If we have more than 1 zones to crop
     if set_param.multiple.isChecked():
-        # Naming according to zones
-        experience_name_zone = experience_name+' Zone '+str(chr(j+64))
-        graph = 'graph(zone_'+str(chr(j+64))+').png'
-        if j == 1:
-            zones = Image.open(input_loc)
-            zones = zones.convert("RGB")
-            Z1 = ImageDraw.Draw(zones)
-            Z1.text((left, upper), chr(64+j), font=font, fill='red')
-            zones.save(directory+'ZonesIdentification.png')
-        else:
-            zones = Image.open(directory+'ZonesIdentification.png')
-            Z1 = ImageDraw.Draw(zones)
-            Z1.text((left, upper), chr(64+j), font=font, fill='red')
-            zones.save(directory + 'ZonesIdentification.png')
+        nbZones = int(set_param.data[9])
     else:
-        # Default name
-        graph = 'graph_'+str(datetime.date.today())+'.png'
-        experience_name_zone = experience_name
+        nbZones = 1
 
-    # For loop that will do the same cropping for each photos
-    for i in set_param.wavelengths:
-        input_loc = directory + str(i) + '.tiff'
-        output_loc = directory + 'cropped\\' + 'crop_' + str(i) + 'nm.png'
+    for j in range(nbZones):
+        # Variables
+        j = j+1
+        avg = []
+        wavelength = []
 
+        # Directories
+        input_loc = directory+str(firstW)+'.tiff'
+        try:
+            os.mkdir(directory + 'cropped')
+        except FileExistsError:
+            pass
+        output_loc = directory+'cropped\\'+'crop_'+str(firstW)+'nm.png'
+
+        # Setup and opening of the selection window
+        screen, px = setup(input_loc)
+        left, upper, right, lower = SelectionWindow(screen, px)
+
+        # Readjust the coordinates if they are reversed
+        if right < left:
+            left, right = right, left
+        if lower < upper:
+            lower, upper = upper, lower
+
+        # Doing the cropping
         im = Image.open(input_loc)
         im = im.crop((left, upper, right, lower))
+
+        # Exit the selection window
+        pygame.display.quit()
+
+        # Save the image in the output location
         im.save(output_loc)
 
-        image = np.array(im)
-        avg.append(np.average(image))
-        wavelength.append(i)
+        # Multiple zones identification on separate image
+        if set_param.multiple.isChecked():
+            # Naming according to zones
+            experience_name_zone = experience_name+' Zone '+str(chr(j+64))
+            graph = 'graph(zone_'+str(chr(j+64))+').png'
+            if j == 1:
+                zones = Image.open(input_loc)
+                zones = zones.convert("RGB")
+                Z1 = ImageDraw.Draw(zones)
+                Z1.text((left, upper), chr(64+j), font=font, fill='red')
+                zones.save(directory+'ZonesIdentification.png')
+            else:
+                zones = Image.open(directory+'ZonesIdentification.png')
+                Z1 = ImageDraw.Draw(zones)
+                Z1.text((left, upper), chr(64+j), font=font, fill='red')
+                zones.save(directory + 'ZonesIdentification.png')
+        else:
+            # Default name
+            graph = 'graph_'+str(datetime.date.today())+'.png'
+            experience_name_zone = experience_name
 
-    # Create the graphic
-    plt.plot(wavelength, avg, marker='o', label=experience_name_zone)
-    plt.title(experience_name+" ("+str(datetime.date.today())+")")
-    plt.grid()
-    plt.xlabel("Wavelength (nm)")
-    plt.ylabel("Intensity")
-    plt.legend(loc='upper right')
+        # For loop that will do the same cropping for each photos
+        for i in set_param.wavelengths:
+            input_loc = directory + str(i) + '.tiff'
+            output_loc = directory + 'cropped\\' + 'crop_' + str(i) + 'nm.png'
 
-# Save and show the graph
-plt.savefig(directory+graph)
-plt.show()
+            im = Image.open(input_loc)
+            im = im.crop((left, upper, right, lower))
+            im.save(output_loc)
+
+            image = np.array(im)
+            avg.append(np.average(image))
+            wavelength.append(i)
+
+        # Create the graphic
+        plt.plot(wavelength, avg, marker='o', label=experience_name_zone)
+        plt.title(experience_name+" ("+str(datetime.date.today())+")")
+        plt.grid()
+        plt.xlabel("Wavelength (nm)")
+        plt.ylabel("Intensity")
+        plt.legend(loc='upper right')
+
+    # Save and show the graph
+    plt.savefig(directory+graph)
+    plt.show()
+
+
+Banc_Optique()
